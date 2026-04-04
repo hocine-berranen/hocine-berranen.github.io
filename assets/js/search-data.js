efcea11d137b5455c4848e4a910d15ff5e057057
hocine-berranen <hocine.berranen1@gmail.com>
2026-04-04 22:14:29 +0000

    Deploying to gh-pages from @ hocine-berranen/hocine-berranen.github.io@6a82cec9df78bfae68fb619b0f7ab060b56ce1c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -76,9 +76,9 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-tempo",
-          title: 'tempo',
-          description: "A modular C command-line tool for time series analysis",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
