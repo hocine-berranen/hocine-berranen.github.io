3004ac60c4626a44ebb9f5b73abc364d889ee56c
hocine-berranen <hocine.berranen1@gmail.com>
2026-05-25 18:04:54 +0000

    Deploying to gh-pages from @ hocine-berranen/hocine-berranen.github.io@cd5b664188988bad6b6c64c840b83b24164fc082 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,51 +46,26 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-tempo-application",
+          title: 'Tempo Application',
+          description: "A modular C application for reading, validating, analyzing, and visualizing time series data. It supports interpolation, JSON input, and Gnuplot script generation.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-devops-ci-cd-pipeline",
+          title: 'DevOps CI/CD Pipeline',
+          description: "A complete CI/CD pipeline that automates AWS EC2 server provisioning, Docker application deployment with Terraform, and continuous integration through GitHub Actions.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-simulated-mini-network-and-services",
+          title: 'Simulated Mini-Network and Services',
+          description: "A simulated mini-network configured with subnets, routers, and DHCP, DNS, and HTTP services, with network traffic analysis using Wireshark.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-vehicle-rental-website",
+          title: 'Vehicle Rental Website',
+          description: "A vehicle rental website with an interactive interface for managing customers, vehicles, rentals, invoice generation, and inventory tracking.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-a-modular-c-tool-for-time-series-analysis",
-          title: 'A Modular C Tool for Time Series Analysis',
-          description: "A modular C application for reading, validating, analyzing, and visualizing time series data.It supports interpolation, JSON input, and Gnuplot script generation.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
